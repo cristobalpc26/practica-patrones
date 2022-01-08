6ea87e970fc9a45a67d515a9d90f666af09bdd94
cristobalpc26 <93983801+cristobalpc26@users.noreply.github.com>
2022-01-08 14:09:48 +0100

    Add files via upload
    First commit

---
+++ StockResourcePlanning/src/main/java/db_scripts/inserciones.sql
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  cgpc2
+ * Created: 08-ene-2022
+ */
+
+INSERT INTO EMPLEADO VALUES 
+('11111111A', 'Pepe', 'Garcia Perez', 'admincc@market.com', 'admin', 'Mañana', '123123123', 'Admin'),
+('22222222B', 'Pedro', 'Fernandez Ayuso', 'pedro.fernandez.ayuso@market.com', 'pedro', 'Tarde', '111111111', 'Empleado');
+
+INSERT INTO PRODUCTO VALUES
+('0', 'Bebida', 'Refrescos', 'Cola', 'Coca-Cola', 1.5, 40, 'EEUU', '2022-01-08', '2025-01-01', 'Pasillo 2');
+
+INSERT INTO EMPLEADO_PRODUCTOS VALUES
+('0', '22222222B', 20, '2022-01-08 12:30:15');
